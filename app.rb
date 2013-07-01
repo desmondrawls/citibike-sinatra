d5d93386415060956051a7c1a22a8a04e5ec8442
Desmond Rawls <captaingrover@gmail.com>
2013-06-30 22:34:56 -0400

    all done

--- app.rb
+++ app.rb
@@ -18,5 +18,21 @@ module Citibike
       erb :home
     end
 
+    get '/form' do
+      erb :form
+    end
+
+    post '/map' do
+      @start = params[:start]
+      @end = params[:end]
+      @start_station = @data.select{|station| station["name"] == @start}.first
+      @end_station = @data.select{|station| station["name"] == @end}.first
+      @start_lat = @start_station["lat"]/1000000.to_f
+      @start_lng = @start_station["lng"]/1000000.to_f
+      @end_lat = @end_station["lat"]/1000000.to_f
+      @end_lng = @end_station["lng"]/1000000.to_f
+      erb :map
+    end
+
   end
 end
